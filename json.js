0080cf648c333f2d2236e5d04d1d5b60db48c012
Yuri Martins <cyuri.martins@gmail.com>
2018-10-04 21:12:53 -0300

    Create json.js

---
+++ json.js
@@ -0,0 +1,22 @@
+
+var objeto = [{
+	 "nome": "Cliente 1",
+	 "posicionamento" : "triagem"
+	},
+	{
+	 "nome": "Cliente 2",
+	 "posicionamento" : "triagem"
+	},
+	{
+	 "nome": "Cliente 3",
+	 "posicionamento" : "triagem"
+	},
+	{
+	 "nome": "Cliente 4",
+	 "posicionamento" : "geral"
+	},
+	{
+	 "nome": "Cliente 5",
+	 "posicionamento" : "emergencia"
+	},
+]
